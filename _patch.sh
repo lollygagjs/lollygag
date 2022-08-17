0ace7211b53c2d1642d2240e4c8e230f8f943260
jeg <jeg@bakeshop>
2022-08-17 16:36:18 +0800

    ...

--- _patch.sh
+++ _patch.sh
@@ -1,8 +1,18 @@
 #! /bin/sh
 
+# install jq if not already installed
+if ! command -v jq >/dev/null; then
+    sudo apt install -y jq
+fi
+
 issemver() {
     [[ $1 =~ ^v[0-9]+\.+[0-9]+\.+[0-9]+(\-[0-9A-Za-z]+)?$ ]]
 }
+
+# TODO:
+# yarn version --patch --no-git-tag-version
+# this will disable automatic commits of updated package.json files
+# add a way to make git commit changes with the message `v<$newver>`
 
 for dir in "${1:-packages}"/*/; do
     (
@@ -10,8 +20,10 @@ for dir in "${1:-packages}"/*/; do
         && lastmsg=$(git log -1 --pretty=%B .) \
         && echo "$dir $lastmsg" \
         && \
-        if ! issemver "$lastmsg";
-        then yarn version --patch --no-git-tag-version && yarn publish
+        if ! issemver "$lastmsg"; then
+            yarn version --patch \
+            && newver=$(cat package.json | jq -r .version) `# get new version from package.json` \
+            && yarn publish --new-version "$newver"
         else echo "No changes to publish";
         fi
     )
