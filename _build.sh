82686f857614c9bc5c8a7b1cad3100b2dd068bf6
jeg <jeg@bakeshop>
2022-08-18 13:22:52 +0800

    ...

--- _build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-for dir in "${1:-packages}"/*/; do
-  ( cd "$dir" && echo "$dir" && yarn build )
-done
--- _tsc.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-for dir in "${1:-packages}"/*/; do
-  ( cd "$dir" && echo "$dir" && npx tsc -d )
-done
