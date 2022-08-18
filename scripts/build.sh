17fd6fab53aaaee750fe34ee78c239d3d3aded19
jeg <jeg@bakeshop>
2022-08-18 16:12:45 +0800

    ...

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 for dir in packages/*/; do
-  (cd "$dir" && echo "$dir" && yarn build && echo "Done!" && exit 0)
+    (cd "$dir" && echo "$dir" && yarn build && echo "Done!")
 done
--- scripts/tsc.sh
+++ scripts/tsc.sh
@@ -1,5 +1,24 @@
 #! /bin/sh
 
+pkgs=
+
 for dir in packages/*/; do
-  (cd "$dir" && echo "$dir" && npx tsc)
+    pkgs+="$(basename "$dir") "
 done
+
+pkgsarray=($(echo "$pkgs"))
+
+isvalidarg() {
+    [[ " ${pkgsarray[*]} " = *" $1 "* ]]
+}
+
+# Exit if argument is empty or invalid
+if ! isvalidarg "$1"; then
+    echo "Invalid package name: ${1-<none>}"
+    echo "Accepted: ${pkgsarray[*]}"
+    echo "Sample usage: $(basename "$0") ${pkgsarray[2]}"
+    echo "Exiting..."
+    exit 0
+fi
+
+(cd "packages/$1" || exit 0 && yarn start)
