54c874e0f0b7668a2194b623baa10b0dba528346
jeg <jeg@bakeshop>
2022-08-18 15:37:26 +0800

    ...

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-for dir in "${1:-packages}"/*/; do
-  ( cd "$dir" && echo "$dir" && yarn build )
+for dir in packages/*/; do
+  (cd "$dir" && echo "$dir" && yarn build && echo "Done!" && exit 0)
 done
--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,37 +5,37 @@ if ! command -v jq >/dev/null; then
     sudo apt install -y jq
 fi
 
-# List of valid arguments
+# Array of valid arguments
 valid=(
-    --major,
-    --minor,
-    --patch,
-    --premajor,
-    --preminor,
-    --prepatch,
+    --major
+    --minor
+    --patch
+    --premajor
+    --preminor
+    --prepatch
     --prerelease
 )
 
-# Check if `$1` (string literal) is found in `$valid`
+# Checks if `$1` (string literal) is found in `$valid`
 isvalidarg() {
-    [[ "${valid[@]}" = $1 ]]
+    [[ " ${valid[*]} " = *" $1 "* ]]
 }
 
 # Exit if argument is empty or invalid
 if ! isvalidarg "$1"; then
     echo "Invalid argument: ${1-<none>}"
-    echo "Accepted values are ${valid[@]}"
-    echo "Sample usage: $(basename $0) ${valid[2]}"
+    echo "Accepted: ${valid[*]}"
+    echo "Sample usage: $(basename "$0") ${valid[2]}"
     echo "Exiting..."
     exit 0
 fi
 
-# List of uncommitted and untracked files in git
+# Array of uncommitted and untracked files in git
 gitdirt=($(git ls-files -o -m --exclude-standard))
 
 # Checks if `$1` (pattern or string literal) is found in `$gitdirt`
 isdirty() {
-    [[ "${gitdirt[@]}" = $1 ]]
+    [[ " ${gitdirt[*]} " =~ $1 ]]
 }
 
 # Check if `$1` is a valid `v` preceded semver
@@ -49,10 +49,9 @@ echo "$sep"
 
 for dir in packages/*/; do
     (
-        cd "$dir"
-
+        cd "$dir" || exit
+        # Last commit message for current `$dir`
         lastmsg=$(git log -1 --pretty=%B .)
-
         echo "$dir $lastmsg"
 
         # Skip if current `$dir` has uncommitted changes
@@ -66,12 +65,10 @@ for dir in packages/*/; do
         if issemver "$lastmsg"; then
             echo "No changes since last publish. Skipping..."
         else
-            echo "Will version and publish..."
-            # yarn version $1 --no-git-tag-version
-            # newver=$(cat package.json | jq -r .version) # Get new version from package.json
-            # yarn publish --new-version "$newver"
-            # git add "${dir}package.json"
-            # git commit -m "v$newver"
+            yarn version "$1" --no-git-tag-version
+            newver=$(cat <package.json | jq -r .version) # New version from package.json
+            git add "${dir}package.json" && git commit -m "v$newver"
+            yarn publish --new-version "$newver"
         fi
 
         echo "$sep"
