fa8cb2d785ad0c19a2a7aa2d69f54fffbf3563ec
jeg <jeg@bakeshop>
2022-08-20 11:17:32 +0800

    keep original and extname fixes

--- packages/postcss/dist/index.js
+++ packages/postcss/dist/index.js
@@ -19,39 +19,41 @@ const postcss_1 = __importDefault(require("postcss"));
 const core_1 = require("@lollygag/core");
 function postcss(options) {
     return function postcssWorker(files) {
+        var _a, _b, _c;
         return __awaiter(this, void 0, void 0, function* () {
             if (!files)
                 return;
-            const { newExtname, targetExtnames, keepOriginal, plugins, processOptions, } = options !== null && options !== void 0 ? options : {};
-            const _newExtname = newExtname !== null && newExtname !== void 0 ? newExtname : '.css';
-            const _targetExtnames = targetExtnames !== null && targetExtnames !== void 0 ? targetExtnames : ['.css', '.pcss'];
-            const _keepOriginal = keepOriginal !== null && keepOriginal !== void 0 ? keepOriginal : true;
+            const { plugins, processOptions } = options !== null && options !== void 0 ? options : {};
+            const defaultExtname = '.css';
+            const newExtname = (_a = options === null || options === void 0 ? void 0 : options.newExtname) !== null && _a !== void 0 ? _a : defaultExtname;
+            const targetExtnames = (_b = options === null || options === void 0 ? void 0 : options.targetExtnames) !== null && _b !== void 0 ? _b : ['.css', '.pcss'];
+            const keepOriginal = (_c = options === null || options === void 0 ? void 0 : options.keepOriginal) !== null && _c !== void 0 ? _c : true;
+            const makeNewFile = keepOriginal && newExtname !== defaultExtname;
             const promises = files.map((file) => __awaiter(this, void 0, void 0, function* () {
-                var _a;
+                var _d;
                 let _file = file;
-                if (_newExtname && _keepOriginal)
+                if (makeNewFile)
                     _file = Object.assign({}, file);
-                if (!_targetExtnames.includes((0, path_1.extname)(_file.path))
+                if (!targetExtnames.includes((0, path_1.extname)(_file.path))
                     || (0, core_1.fullExtname)(_file.path).endsWith('.min.css')) {
                     return;
                 }
                 const existingSourcemap = files.find((f) => f.path === (0, path_1.join)(`${file.path}.map`));
                 let filePath = _file.path;
-                if (_newExtname !== false) {
-                    filePath = (0, core_1.changeExtname)(_file.path, _newExtname);
+                if (newExtname !== false) {
+                    filePath = (0, core_1.changeExtname)(_file.path, newExtname);
                 }
-                const result = yield (0, postcss_1.default)(plugins).process((_a = _file.content) !== null && _a !== void 0 ? _a : '', Object.assign({ from: file.path, to: filePath, map: {
+                const result = yield (0, postcss_1.default)(plugins).process((_d = _file.content) !== null && _d !== void 0 ? _d : '', Object.assign({ from: file.path, to: filePath, map: {
                         inline: false,
                         prev: existingSourcemap ? existingSourcemap.content : false,
                     } }, processOptions));
                 _file.path = filePath;
                 _file.content = result.css;
-                if ((options === null || options === void 0 ? void 0 : options.newExtname) && _keepOriginal) {
+                if (makeNewFile)
                     files.push(_file);
-                }
                 if (result.map) {
-                    if (options === null || options === void 0 ? void 0 : options.newExtname) {
-                        if (!_keepOriginal && existingSourcemap) {
+                    if (newExtname) {
+                        if (!keepOriginal && existingSourcemap) {
                             existingSourcemap.path = (0, path_1.join)(`${filePath}.map`);
                             existingSourcemap.content = result.map.toString();
                         }
--- packages/postcss/src/index.ts
+++ packages/postcss/src/index.ts
@@ -15,25 +15,21 @@ export function postcss(options?: IOptions): TWorker {
     return async function postcssWorker(this: TWorker, files): Promise<void> {
         if(!files) return;
 
-        const {
-            newExtname,
-            targetExtnames,
-            keepOriginal,
-            plugins,
-            processOptions,
-        } = options ?? {};
+        const {plugins, processOptions} = options ?? {};
 
-        const _newExtname = newExtname ?? '.css';
-        const _targetExtnames = targetExtnames ?? ['.css', '.pcss'];
-        const _keepOriginal = keepOriginal ?? true;
+        const defaultExtname = '.css';
+        const newExtname = options?.newExtname ?? defaultExtname;
+        const targetExtnames = options?.targetExtnames ?? ['.css', '.pcss'];
+        const keepOriginal = options?.keepOriginal ?? true;
+        const makeNewFile = keepOriginal && newExtname !== defaultExtname;
 
         const promises = files.map(async(file) => {
             let _file = file;
 
-            if(_newExtname && _keepOriginal) _file = {...file};
+            if(makeNewFile) _file = {...file};
 
             if(
-                !_targetExtnames.includes(extname(_file.path))
+                !targetExtnames.includes(extname(_file.path))
                 || fullExtname(_file.path).endsWith('.min.css')
             ) {
                 return;
@@ -45,8 +41,8 @@ export function postcss(options?: IOptions): TWorker {
 
             let filePath = _file.path;
 
-            if(_newExtname !== false) {
-                filePath = changeExtname(_file.path, _newExtname);
+            if(newExtname !== false) {
+                filePath = changeExtname(_file.path, newExtname);
             }
 
             const result = await pcss(plugins).process(_file.content ?? '', {
@@ -62,13 +58,11 @@ export function postcss(options?: IOptions): TWorker {
             _file.path = filePath;
             _file.content = result.css;
 
-            if(options?.newExtname && _keepOriginal) {
-                files.push(_file);
-            }
+            if(makeNewFile) files.push(_file);
 
             if(result.map) {
-                if(options?.newExtname) {
-                    if(!_keepOriginal && existingSourcemap) {
+                if(newExtname) {
+                    if(!keepOriginal && existingSourcemap) {
                         existingSourcemap.path = join(`${filePath}.map`);
                         existingSourcemap.content = result.map.toString();
                     } else {
