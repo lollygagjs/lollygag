a4a444db8280872251a79c9bf39be413fb6d0613
jeg <jeg@bakeshop>
2022-08-22 23:03:15 +0800

    changed do:tsc script

--- scripts/do/tsc.sh
+++ scripts/do/tsc.sh
@@ -1,24 +1,5 @@
 #! /bin/sh
 
-pkgs=
-
 for dir in packages/*/; do
-    pkgs+="$(basename "$dir") "
+    (cd "$dir" && echo "$dir" && npx tsc -d && echo "Done!")
 done
-
-pkgsarray=($(echo "$pkgs"))
-
-isvalidarg() {
-    [[ " ${pkgsarray[*]} " = *" $1 "* ]]
-}
-
-# Exit if argument is empty or invalid
-if ! isvalidarg "$1"; then
-    echo "Invalid package name: ${1-<none>}"
-    echo "Accepted: ${pkgsarray[*]}"
-    echo "Sample usage: $(basename "$0") ${pkgsarray[2]}"
-    echo "Exiting..."
-    exit
-fi
-
-(cd "packages/$1" || exit && yarn start)
