8ea1ef5e76276bbae03ca7bd70dbc91d911204a6
jeg <jeg@bakeshop>
2022-08-22 23:02:45 +0800

    added do:watch script

---
+++ scripts/do/watch.sh
@@ -0,0 +1,24 @@
+#! /bin/sh
+
+pkgs=
+
+for dir in packages/*/; do
+    pkgs+="$(basename "$dir") "
+done
+
+pkgsarray=($(echo "$pkgs"))
+
+isvalidarg() {
+    [[ " ${pkgsarray[*]} " = *" $1 "* ]]
+}
+
+# Exit if argument is empty or invalid
+if ! isvalidarg "$1"; then
+    echo "Invalid package name: ${1-<none>}"
+    echo "Accepted: ${pkgsarray[*]}"
+    echo "Sample usage: $(basename "$0") ${pkgsarray[2]}"
+    echo "Exiting..."
+    exit
+fi
+
+(cd "packages/$1" || exit && yarn start)
