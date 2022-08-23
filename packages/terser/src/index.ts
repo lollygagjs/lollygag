470bf1b99f5dc9510b2c2303155678c823c9dc3a
jeg <jeg@bakeshop>
2022-08-23 22:02:22 +0800

    cleanup

--- packages/terser/src/index.ts
+++ packages/terser/src/index.ts
@@ -14,37 +14,41 @@ export default function terser(options?: IOptions): TWorker {
     return async function terserWorker(this: TWorker, files): Promise<void> {
         if(!files) return;
 
-        const {minifyOptions} = options ?? {};
         const defaultExtname = '.js';
-        const newExtname = options?.newExtname ?? defaultExtname;
-        const targetExtnames = options?.targetExtnames ?? ['.js'];
-        const keepOriginal = options?.keepOriginal ?? true;
+
+        const {
+            minifyOptions,
+            newExtname = defaultExtname,
+            targetExtnames = ['.js'],
+            keepOriginal = true,
+        } = options ?? {};
+
         const makeNewFile = keepOriginal && newExtname !== defaultExtname;
 
-        const promises = files.map(async(file) => {
-            let _file = file;
+        const promises = files.map(async(f) => {
+            let file = f;
 
-            if(makeNewFile) _file = deepCopy(file);
+            if(makeNewFile) file = deepCopy(f);
 
             if(
-                !targetExtnames.includes(extname(_file.path))
-                || fullExtname(_file.path).endsWith('.min.js')
+                !targetExtnames.includes(extname(file.path))
+                || fullExtname(file.path).endsWith('.min.js')
             ) {
                 return;
             }
 
-            let filePath = _file.path;
+            let filePath = file.path;
 
             if(newExtname !== false) {
-                filePath = changeExtname(_file.path, newExtname);
+                filePath = changeExtname(file.path, newExtname);
             }
 
-            const {code} = await t.minify(_file.content ?? '', minifyOptions);
+            const {code} = await t.minify(file.content ?? '', minifyOptions);
 
-            _file.path = filePath;
-            _file.content = code;
+            file.path = filePath;
+            file.content = code;
 
-            if(makeNewFile) files.push(_file);
+            if(makeNewFile) files.push(file);
         });
 
         await Promise.all(promises);
